39e6ef53fda97f47035bf0a1c5e76c22e8cd5315
Mason Galland <masongalland@me.com>
2017-07-03 13:26:42 -0600

    initial setup

---
+++ src/components/ColorChanger.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class ColorChanger extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allowEdit: this.props.allowEdit
+    };
+  }
+
+  // componentWillReceiveProps
+
+  render() {
+    return (
+      <select className="dropDownContainer">
+        <option value="black"> Black </option>
+        <option value="blue"> Blue </option>
+        <option value="green"> Green </option>
+      </select>
+    )
+  }
+}
